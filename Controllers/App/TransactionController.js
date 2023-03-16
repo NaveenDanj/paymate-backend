288a193786838df4d2bb67a712a3031c3c2f4908
Naveen Hettiwaththa <naveenhettiwaththa@gmail.com>
2023-03-16 13:33:11 +0530

    added transaction model and add fund transfer endpoint

--- Controllers/App/TransactionController.js
+++ Controllers/App/TransactionController.js
@@ -2,8 +2,53 @@ const express = require("express");
 const router = express.Router();
 const Joi = require("joi");
 const { getTransactionStatus } = require("../../Services/PaypalSDKService");
+const Wallet = require("../../Models/Wallet");
 
-router.post("/fund-transfer");
+router.post("/fund-transfer", async (req, res) => {
+  let validator = Joi.object({
+    toWalletId: Joi.string().required(),
+    amount: Joi.number().required(),
+  });
+
+  try {
+    const data = await validator.validateAsync(req.body, { abortEarly: false });
+    let user = req.user;
+    let wallet = await Wallet.findOne({ userId: user._id });
+    let toWallet = await Wallet.findOne({ _id: data.toWalletId });
+
+    if (!toWallet) {
+      return res.status(400).json({
+        message: "Error while processing the transaction! Wallet not found.",
+      });
+    }
+
+    if (data.amount < 0) {
+      return res.status(400).json({
+        message: "Error while processing the transaction! Invalid amount.",
+      });
+    }
+
+    if (!wallet) {
+      return res.status(400).json({
+        message: "Error while processing the transaction! Wallet not found.",
+      });
+    }
+
+    if (wallet.Balance < data.amount) {
+      return res.status(400).json({
+        message:
+          "Error while processing the transaction! Insufficient balance.",
+      });
+    }
+
+    // process the transaction
+  } catch (err) {
+    return res.status(400).json({
+      message: "Error while processing the transaction!",
+      error: err,
+    });
+  }
+});
 router.post("/qr-pay");
 
 router.post("/qr-init-payment");
---
+++ Models/Transaction.js
@@ -0,0 +1,63 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const TransactionSchema = new Schema(
+  {
+    fromUserId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+
+    fromWalletId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: "Wallet",
+    },
+
+    type: {
+      type: String,
+      default: "created",
+      enum: [
+        "fund-transfer",
+        "instance-pay",
+        "qr-pay",
+        "near-pay",
+        "token-pay",
+      ],
+    },
+
+    userRemarks: {
+      type: String,
+      required: true,
+    },
+
+    receiverRemarks: {
+      type: String,
+      required: true,
+    },
+
+    status: {
+      type: String,
+      default: "created",
+      enum: [
+        "created",
+        "quied",
+        "processing",
+        "pending",
+        "success",
+        "failed",
+        "bloked",
+      ],
+    },
+
+    amount: {
+      type: Number,
+      required: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const Transaction = mongoose.model("Transaction", TransactionSchema);
+module.exports = Transaction;
